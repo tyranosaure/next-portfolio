6df468f3e7e65cee27aee4ab244d60d7b8a2a82d
Ayoub <abassou643@gmail.com>
2023-03-27 10:30:46 +0200

    add Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import "styles/globals.scss"
 import "styles/backgroundStyles.scss"
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Component {...pageProps} />
+			<Analytics />
+		</>
+	)
 }
 
 export default MyApp
