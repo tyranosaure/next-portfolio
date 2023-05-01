1a334e00c24619bbdee93cca0edcf2aec1868d0c
Tyranosaure <abassou642@gmail.com>
2023-05-01 13:31:10 +0200

    Update color

--- components/BackgroundCircles.js
+++ components/BackgroundCircles.js
@@ -16,8 +16,8 @@ const BackgroundCircles = ({ children }) => {
 					className="relative flex items-center justify-center"
 				>
 					<div className="absolute border-2 border-t-[15px] border-l-[15px] border-white/10 bg-[#1D1D1D] rounded-full h-[80dvh] w-[80dvh] ml-0 mt-0 animate-spin-slow" />
-					<div className="absolute border-2 border-b-[15px] border-r-[15px] border-orange-700 bg-orange-900  rounded-full h-[60dvh] w-[60dvh] animate-spin-slow mb-10" />
-					<div className="absolute border-4 border-t-[15px] border-l-[15px] bg-yellow-600 rounded-full h-[40dvh] w-[40dvh] animate-spin-slow" />
+					<div className="absolute border-2 border-b-[15px] border-r-[15px] border-yellow-600/30 bg-yellow-700/30  rounded-full h-[60dvh] w-[60dvh] animate-spin-slow mb-10" />
+					<div className="absolute border-4 border-t-[15px] border-l-[15px] bg-yellow-600/30 rounded-full h-[40dvh] w-[40dvh] animate-spin-slow" />
 					{children}
 				</motion.div>
 			)}
